1de1100ffba00cdd61e5fce87e3ad4de4544c65c
birdpersson <aleksandarvujasin@gmail.com>
2020-11-17 21:35:41 +0100

    Initial project

---
+++ src/test/java/com/isa59/isa/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isa59.isa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
